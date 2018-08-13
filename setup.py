2e94d47a878e67db032153b47bb4fa24c3f50b4c
marcel zoll <marcel.zoll@nowinteract.com>
2018-08-13 11:35:09 +0200

    remodel the foldermodel a bit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='common_tools',
+      version='0.1.3',
+      description='common tools that gonna be reused in some projects',
+      url='http://github.com/mzoll/common_tools',
+      author='Marcel Zoll',
+      author_email='marcel.zoll.physics@gmail.com',
+      license='',
+      packages=['common_tools'],
+      zip_safe=False)
