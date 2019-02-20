15e2ce23e6757f00b0597c07ff462f6b4accc825
mzoll <marcel.zoll.physics@gmail.com>
2019-02-20 16:47:13 +0100

    better pyodbc credentials parsing

--- common_tools/pyodbc.py
+++ common_tools/pyodbc.py
@@ -1,11 +1,22 @@
-'''
-Created on July 9, 2018
+"""
+Created on Feb 15, 2019
 
 @author: marcel.zoll
-'''
+"""
 
-import pyodbc
+class PyodbcCredentials(object):
+    def __init__(self, dsn, db, uid, pwd=None):
+        self.dsn = dsn
+        self.db = db
+        self.uid = uid
+        self.pwd = pwd
 
-def pyodbcConnectString(dsn ,db, uid, pwd):
-    """ compose a very simple pyodb connection string """
-    return "DSN={};DATABASE={};UID={};PWD={}".format(dsn ,db , uid, pwd)
+    def connectionString(self):
+        s = "DSN={};DATABASE={};UID={};".format(self.dsn, self.db, self.uid, self.pwd)
+        if self.pwd is not None:
+            s += "PWD={};".format(self.pwd)
+        return s
+
+
+def pyodbcConnectionString(dsn, db, uid, pwd):
+    return "DSN={};DATABASE={};UID={};PWD={}".format(dsn, db, uid, pwd)
